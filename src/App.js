06c0e1cdbd099dbfd6ae81b232359a50b6ccdfc1
douguohai <douguohai@163.com>
2023-08-06 21:52:40 +0800

    [add] 测试项目

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import {LiveKitRoom, VideoConference} from '@livekit/components-react';
+import "@livekit/components-styles";
+import {useMount} from 'ahooks';
+import {useState} from "react";
+import axios from "axios";
+
+
+const wsURL = "ws://127.0.0.1:7880"
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTEyMjA5ODAsImlzcyI6ImRldmtleSIsIm5iZiI6MTY5MTIxNzM4MCwic3ViIjoiMjEiLCJ2aWRlbyI6eyJyb29tIjoiMSIsInJvb21Kb2luIjp0cnVlfX0.P2zEMBVDBgKKQ6BHcuXXGsPcIjs2sORTNRm5WlwVIwQ"
+
+const BaseUrl = "http://127.0.0.1:8080"
+
+function getToken(userId, roomId) {
+    // 向给定ID的用户发起请求
+    axios.get(BaseUrl + "/getToken/" + roomId + "/" + userId)
+        .then(function (response) {
+            // 处理成功情况
+            console.log(response);
+        })
+        .catch(function (error) {
+            // 处理错误情况
+            console.log(error);
+        })
+        .then(function () {
+            // 总是会执行
+        });
+}
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [token2, setToken] = useState("");
+    const [roomId, setRoomId] = useState("");
+
+    useMount(() => {
+        console.log("mount", new Date())
+        getToken(1, 2)
+    });
+
+
+    return (
+        <LiveKitRoom token={token} serverUrl={wsURL} connect={true}>
+            <VideoConference/>
+        </LiveKitRoom>
+    );
 }
 
 export default App;
